f3b6648ffd8d961feec1252967ce0bb998b7e277
mmingareev <amingaraev@smartdatatech.io>
2022-09-16 19:41:22 +0300

    settings

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
 import store from './store'
-
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/tasks',
+    name: 'Tasks',
+    component: () => import('@/pages/Task')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
